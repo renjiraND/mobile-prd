e854bb1606400865795deee187b1db787453747f
mnurraihann <raihannaf@gmail.com>
2019-02-24 22:33:27 +0700

    unlock with gesture done

--- app/src/main/java/com/renrairah/bukalock/UnlockFragment.java
+++ app/src/main/java/com/renrairah/bukalock/UnlockFragment.java
@@ -1,8 +1,10 @@
 package com.renrairah.bukalock;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +14,8 @@ import android.view.ViewGroup;
  * A simple {@link Fragment} subclass.
  */
 public class UnlockFragment extends Fragment {
+
+    private CardView gamesCardView, motionCardView;
 
     public static UnlockFragment newInstance() {
         UnlockFragment fragment = new UnlockFragment();
@@ -26,8 +30,24 @@ public class UnlockFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_unlock, container, false);
+        gamesCardView = (CardView) rootView.findViewById(R.id.card_games);
+        motionCardView = (CardView) rootView.findViewById(R.id.card_motion);
+        gamesCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        motionCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), UnlockGestureActivity.class);
+                startActivity(intent);
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_unlock, container, false);
+        return rootView;
     }
 
 }
---
+++ app/src/main/java/com/renrairah/bukalock/UnlockGestureActivity.java
@@ -0,0 +1,110 @@
+package com.renrairah.bukalock;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Timer;
+
+import static java.lang.Math.abs;
+
+public class UnlockGestureActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private TextView txtStatus;
+    private ImageView imgStatus;
+    private int status = 0; // 0 = idle, 1 = flat position, 2 = accelerated forward and waiting to be rotated, 2 = rotated and unlocked
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unlock_gesture);
+        txtStatus = (TextView) findViewById(R.id.txtStatus);
+        imgStatus = (ImageView) findViewById(R.id.imgStatus);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            getAccelerometer(sensorEvent);
+        }
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            getGyroscope(sensorEvent);
+        }
+    }
+
+    private void getAccelerometer(SensorEvent event) {
+        float[] values = event.values;
+        // Movement
+        float x = values[0];
+        float y = values[1];
+        float z = values[2];
+
+        if (((abs(x) <= 1) && (abs(y) <= 1)) && ((z >= 10) && (status == 0))) {
+            status = 1;
+            txtStatus.setText("Move your phone forward");
+            imgStatus.setImageResource(R.drawable.ic_forward);
+        }
+
+        if (((abs(y) >= 3) && (z >= 10)) && (status == 1)){
+            status = 2;
+            txtStatus.setText("Rotate your phone to the left");
+            imgStatus.setImageResource(R.drawable.ic_rotate);
+        }
+    }
+
+    private void getGyroscope(SensorEvent event) {
+        float[] values = event.values;
+
+        float x = values[0];
+        float y = values[1];
+        float z = values[2];
+
+        if ((y < -2.5f) && (status == 2)){
+            status = 3;
+            txtStatus.setText("Door unlocked!");
+            imgStatus.setImageResource(R.drawable.ic_unlock);
+            sensorManager.unregisterListener(this);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // register this class as a listener for the orientation and
+        // accelerometer sensors
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        // unregister listener
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
