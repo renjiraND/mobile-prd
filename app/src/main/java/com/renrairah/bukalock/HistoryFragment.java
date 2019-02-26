640e6934133f55400a94b2a004073ec3dd5db594
mnurraihann <raihannaf@gmail.com>
2019-02-26 23:21:23 +0700

    connect to firebase database

--- app/src/main/java/com/renrairah/bukalock/HistoryFragment.java
+++ app/src/main/java/com/renrairah/bukalock/HistoryFragment.java
@@ -1,15 +1,29 @@
 package com.renrairah.bukalock;
 
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.LinkedList;
+
+import static android.support.constraint.Constraints.TAG;
 
 
 /**
@@ -42,23 +56,51 @@ public class HistoryFragment extends Fragment {
         recyclerView.setAdapter(adapter);
         createListData();
         // Inflate the layout for this fragment
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(layoutManager);
         return rootView;
     }
 
     private void createListData() {
-        Date date = new Date();
-        History history = new History(2,date,1);
-        historyList.add(history);
-        history = new History(1,date,0);
-        historyList.add(history);
-        history = new History(2,date,1);
-        historyList.add(history);
-        history = new History(1,date,1);
-        historyList.add(history);
-        history = new History(1,date,0);
-        historyList.add(history);
-        history = new History(3,date,1);
-        historyList.add(history);
+        final DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference().child("history");
+        Log.d(TAG, "loading history");
+        final ProgressDialog Dialog = new ProgressDialog(getActivity());
+        Dialog.setMessage("Please wait...");
+        Dialog.show();
+        Query historyQuery = dbRef.orderByChild("date");
+        historyQuery.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot messageSnapshot: dataSnapshot.getChildren()) {
+                        History history = messageSnapshot.getValue(History.class);
+                        Log.d(TAG, "got history " + history.dateToString());
+                        historyList.add(history);
+                        adapter.notifyDataSetChanged();
+                    }
+                    Dialog.hide();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Failed to read value
+            }
+        });
+//        History history = new History(2,date,1);
+//        historyList.add(history);
+//        history = new History(1,date,0);
+//        historyList.add(history);
+//        history = new History(2,date,1);
+//        historyList.add(history);
+//        history = new History(1,date,1);
+//        historyList.add(history);
+//        history = new History(1,date,0);
+//        historyList.add(history);
+//        history = new History(3,date,1);
+//        historyList.add(history);
         adapter.notifyDataSetChanged();
     }
 }
